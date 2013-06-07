b07b105edbc6b931ac5074b841f50afdd9bb51e0
dnaloco <arthur_scosta@yahoo.com.br>
2013-06-07 13:42:25 -0300

    Dando overload no metodo getColumnClass, fazendo a conexao com o bando derek_java_lessons, adicionando rows ao DefaultTableModel e comeando a desenvolver o layout. Estou ainda na metade do video. Terminei por hora, pois vou para o trabalho. Depois Continuo. Entao ainda faltam 23 aulas e meia para terminar esta parte um do meu treinamento java.

--- src/br/org/arthur/lessonThirtySeven/Lesson37.java
+++ src/br/org/arthur/lessonThirtySeven/Lesson37.java
@@ -36,15 +36,61 @@ public class Lesson37 extends JFrame
 			
 			if((column >= 0) && (column < getColumnCount()))
 			{
-				returnValue = getValue(0, column).getClass();
+				returnValue = getValueAt(0, column).getClass();
+			}
+			else
+			{
+				returnValue = Object.class;
 			}
 			
 			return returnValue;
 		}
 	};
 	
+	static JTable table = new JTable(dTableModel);
+	
 	public static void main(String[] args)
 	{
+		new Lesson37();
+	}
+	
+	public Lesson37()
+	{
+		super("Lesson 37 - Derek Banas");
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		populateTableModel();
+	}
+	
+	public static void populateTableModel()
+	{
+		Connection conn = null;
 		
+		try
+		{
+			Class.forName("com.mysql.jdbc.Driver");
+			conn = DriverManager.getConnection("jdbc:mysql://localhost/derek_java_lessons", "arthur", "artdna07");
+			
+			Statement sqlState = conn.createStatement();
+			
+			String sqlQuery = "SELECT first_name, last_name, state, birth FROM president";
+			
+			
+		}
+		catch(ClassNotFoundException e)
+		{
+			System.out.println("Class not fount.");
+			System.out.println("Message: " + e.getMessage());
+			System.exit(0);
+		}
+		catch(SQLException e)
+		{
+			System.out.println("SQL Exception Error: " + e.getMessage());
+			System.out.println("Vendor Code Error:" + e.getErrorCode());
+			System.exit(0);
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
 	}
 }
